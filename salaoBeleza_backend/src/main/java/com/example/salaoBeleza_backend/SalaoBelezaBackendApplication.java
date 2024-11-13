3f64916f55c1fb4883251bddd6e98e231212f2fa
Amanda Pitta <manpitta@gmail.com>
2024-11-13 15:53:36 -0300

    iniciando desenvolvimento

---
+++ salaoBeleza_backend/src/main/java/com/example/salaoBeleza_backend/SalaoBelezaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.salaoBeleza_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoBelezaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoBelezaBackendApplication.class, args);
+	}
+
+}
---
+++ salaoBeleza_backend/src/test/java/com/example/salaoBeleza_backend/SalaoBelezaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salaoBeleza_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoBelezaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
